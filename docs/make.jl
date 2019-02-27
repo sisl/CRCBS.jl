b37a69aeb162033aa8f7ef9e3e283df2be595eb6
kylebrown <kylejbrown17@gmail.com>
2019-02-27 12:19:53 -0800

    more changes

---
+++ docs/make.jl
@@ -0,0 +1,37 @@
+using Documenter,CRCBS
+
+#
+include("src/makeplots.jl")
+
+# This function builds the documentation
+makedocs(
+    modules   = [CRCBS],
+    doctest   = false,
+    clean     = true,
+    linkcheck = false,
+    format    = Documenter.HTML(),
+    sitename  = "CRCBS.jl",
+    authors   = "Kyle Brown",
+    pages     = Any[
+        "Home" => "index.md",
+        "Modules" => Any[
+            "modules/submodule.md", # Use default module name in sidebar
+            "Rubber Ducks" => "modules/rubber_ducks.md", # Rename a module in sidebar
+        ],
+        "Examples" => Any[
+            "Plotting Example" => "examples/plotting_example.md"
+        ],
+        "Library Index" => "library_index.md",
+    ]
+)
+
+# Generate plots
+# Note: Must be called after makedocs so the build folder are created
+makeplots()
+
+deploydocs(
+    repo = "github.com/kylejbrown17/CRCBS.jl",
+    devbranch = "master",
+    devurl = "latest",
+    deps = makeplots,
+)
