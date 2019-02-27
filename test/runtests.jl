f4a46f6d8edc4eb56b3018153726150c2d4616ae
kylebrown <kylejbrown17@gmail.com>
2019-02-27 12:30:12 -0800

    added test infrastructure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,43 @@
-using CRCBS
 using Test
+using Logging
 
-CRCBS.greet()
-@test 1 == 1
+# Package Under Test
+using CRCBS
+
+# Set logging level
+global_logger(SimpleLogger(stderr, Logging.Debug))
+
+# Check equality of two arrays
+@inline function array_isapprox(x::AbstractArray{F},
+                  y::AbstractArray{F};
+                  rtol::F=sqrt(eps(F)),
+                  atol::F=zero(F)) where {F<:AbstractFloat}
+
+    # Easy check on matching size
+    if length(x) != length(y)
+        return false
+    end
+
+    for (a,b) in zip(x,y)
+        @test isapprox(a,b, rtol=rtol, atol=atol)
+    end
+end
+
+# Check if array equals a single value
+@inline function array_isapprox(x::AbstractArray{F},
+                  y::F;
+                  rtol::F=sqrt(eps(F)),
+                  atol::F=zero(F)) where {F<:AbstractFloat}
+
+    for a in x
+        @test isapprox(a, y, rtol=rtol, atol=atol)
+    end
+end
+
+# Define package tests
+@time @testset "CRCBS Package Tests" begin
+    testdir = joinpath(dirname(@__DIR__), "test")
+    @time @testset "CRCBS.MainTests" begin
+        include(joinpath(testdir, "test_main.jl"))
+    end
+end
---
+++ test/test_main.jl
@@ -0,0 +1,4 @@
+let
+    CRCBS.greet()
+    @test 1 == 1
+end
