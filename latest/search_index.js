058e3f61dcc66d31e361f1a13c1364c3c264eefd
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-27 20:36:38 +0000

    build based on cbe0bdb

---
+++ latest/search_index.js
@@ -0,0 +1,83 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#CRCBS.jl-Documentation-1",
+    "page": "Home",
+    "title": "CRCBS.jl Documentation",
+    "category": "section",
+    "text": "Welcome to the CRCBS.jl documentation. The prurpose of this  project is to provide an example package structure which can be easily copied and modified."
+},
+
+{
+    "location": "#Making-A-Custom-Package-Logo-1",
+    "page": "Home",
+    "title": "Making A Custom Package Logo",
+    "category": "section",
+    "text": "To make a custom package logo and include it in the documentation, simply add a  logo.png file to /docs/assets/. For the logo to render well, it is recommended to be 500px by 500px and at least 144dpi."
+},
+
+{
+    "location": "modules/submodule/#",
+    "page": "Submodule",
+    "title": "Submodule",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "modules/submodule/#Submodule-1",
+    "page": "Submodule",
+    "title": "Submodule",
+    "category": "section",
+    "text": "Write Markdown with inline comments to create documentation pages.your_function"
+},
+
+{
+    "location": "modules/rubber_ducks/#",
+    "page": "Rubber Ducks",
+    "title": "Rubber Ducks",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "modules/rubber_ducks/#Rubber-Ducks-1",
+    "page": "Rubber Ducks",
+    "title": "Rubber Ducks",
+    "category": "section",
+    "text": "You can modify the name of both the file in the documentationrubber_ducks_in_earth\nRUBBER_DUCK_PER_SEC"
+},
+
+{
+    "location": "examples/plotting_example/#",
+    "page": "Plotting Example",
+    "title": "Plotting Example",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "examples/plotting_example/#Plotting-Example-1",
+    "page": "Plotting Example",
+    "title": "Plotting Example",
+    "category": "section",
+    "text": "In this example we showcase how to automatically build and generate plots as part of the documentation deployment process.The plots in this example are produced by the file docks/src/makeplots.jl The contents of that file are run each time the documentation is deployed. (Image: )note: Note\nPlots will only appear in documentation when deployed to github pages.warning: Orange Box\nHere is how you add warning box to your documentation"
+},
+
+{
+    "location": "library_index/#",
+    "page": "Library Index",
+    "title": "Library Index",
+    "category": "page",
+    "text": ""
+},
+
+]}
