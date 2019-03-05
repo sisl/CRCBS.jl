530d7aa9a6d1429c54c2d028e094b6e7760aa868
kylebrown <kylejbrown17@gmail.com>
2019-03-05 09:59:28 -0800

    added some types

--- src/CRCBS.jl
+++ src/CRCBS.jl
@@ -5,7 +5,53 @@ using LinearAlgebra
 using NearestNeighbors
 
 export
+    MAPF,
+    CBSConstraint,
+    ConstraintTreeNode,
+    generate_random_grid_graph,
+    generate_random_grid_graph_by_random_walk,
     initialize_grid_graph
+
+"""
+    A MAPF is an instance of a Multi Agent Path Finding problem. It consists of
+    a graph `G` whose edges have unit length, as well as a set of start and goal
+    vertices on that graph
+"""
+struct MAPF # Multi Agent Path Finding Problem
+    G::Graph
+    starts::Vector{Int}
+    goals::Vector{Int}
+end
+
+"""
+    Encodes a conflict between two agents at a particular time
+"""
+struct Conflict
+    agent_id1::Int
+    agent_id2::Int
+    node_id::Int
+    t::Int
+end
+struct CBSConstraint
+    a::Int # agent ID
+    v::Int # vertex ID
+    t::Int # time ID
+end
+struct ConstraintTreeNode
+    constraints::Set{CBSConstraint}
+    solution::Vector{Vector{Int}} # set of paths (one per agent) through graph
+    cost::Int # cost = sum([length(path) for path in solution])
+    parent::Int # index of parent node
+    children::Tuple{Int,Int} # indices of two child nodes
+    # is_goal::Bool
+end
+
+"""
+    The Conflict-Based search algorithm for multi-agent path finding
+"""
+function CBS(mapf::MAPF)
+    # TODO implement!
+end
 
 """
     Initializes a grid graph
